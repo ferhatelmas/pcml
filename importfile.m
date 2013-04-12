90ca10166754aaa64a2105ecf8e2c85403efe63c
Erol Can Ün <erolcan.un@gmail.com>
2013-04-12 15:35:16 +0200

    importfile double conversion

--- importfile.m
+++ importfile.m
@@ -9,6 +9,6 @@ newData1 = load('-mat', fileToRead);
 % Create new variables in the base workspace from those fields
 vars = fieldnames(newData1);
 for i = 1:length(vars)
-    assignin('base', vars{i}, newData1.(vars{i}));
+    assignin('base', vars{i}, double(newData1.(vars{i})));
 end
 
