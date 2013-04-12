1b7265eada31e0ee6c8d6032f8b4331fdb1f2bfa
Erol Can Ün <erolcan.un@gmail.com>
2013-04-12 14:55:58 +0200

    splitdata 'save' function change

--- splitdata.m
+++ splitdata.m
@@ -1,10 +1,10 @@
-function splitdata(dataset)
-%SPLITDATA(train_data)
+function splitdata(dataset,name)
+%SPLITDATA(dataset)
 %  Splits training dataset into two as training (2/3) and validation (1/3)
 %  and saves them into a mat file
 %  dataset:  matrix holding the training data
 
-[num_pixels num_instances] = size(dataset);
+num_instances = size(dataset,2);
 % Create random permutation with 1/3 of total number of instances
 val_columns = randperm(num_instances, num_instances*1/3);
 % Use these random numbers to pick instances for validation dataset
@@ -15,6 +15,6 @@ dataset(:,val_columns) = [];
 train_data = dataset;
 
 % save validation and training datasets to file for later use
-save('val_dataset.m', val_data);
-save('train_dataset.m', train_data);
+save(strcat('val_', name,'.mat'), 'val_data');
+save(strcat('train_', name, '.mat'), 'train_data');
 
