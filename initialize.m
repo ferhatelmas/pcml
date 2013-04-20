377c9cede3b5e079f6e53f7cbaaf151bf06dfaf1
ferhat elmas <elmas.ferhat@gmail.com>
2013-04-20 20:06:31 +0200

    weight initializer, forward pass and fixed a bug in sigmoid

---
+++ initialize.m
@@ -0,0 +1,8 @@
+function matrix = initialize(m, n)
+%INITIALIZE(m, n)
+% initializes a matrix with given size
+% to make gradient descent faster 
+% m = output size
+% n = input size
+    
+matrix = normrnd(0, sqrt(1/(1+n)), m, n);
---
+++ mlp_forward.m
@@ -0,0 +1,27 @@
+function [a_l_1, a_r_1, z_l_1, z_r_1, a_l_2, a_r_2, a_lr_2, z_2, a_3] = mlp_forward(x_l, x_r, w_l_1, b_l_1, w_r_1, b_r_1, w_l_2, b_l_2, w_r_2, b_r_2, w_lr_2, b_lr_2, w_3, b_3)
+%MLP_FORWARD 
+% Implements forward pass of MLP
+%  x_l, x_r: left and right input vectors
+% (w_l_1, b_l_1), (w_r_1, b_r_1): first level initial weight matrices and bias vectors
+% (w_l_2, b_l_2), (w_r_2, b_r_2), (w_lr_2, b_lr_2): second level initial weight matrices and bias vectors
+% (w_3, b_3): last level weight matrice and bias scalar
+
+% first level activation values
+a_l_1 = w_l_1 * x_l + b_l_1;
+a_r_1 = w_r_1 * x_r + b_r_1;
+
+% first level non-linear transforms
+z_l_1 = g_1(a_l_1);
+z_r_1 = g_1(a_r_1);
+z_lr_1 = [z_l_1 z_r_1];
+
+% second level activation values
+a_l_2 = w_l_2 * z_l_1 + b_l_2;
+a_r_2 = w_r_2 * z_r_1 + b_r_2;
+a_lr_2 = w_lr_2 * z_lr_1 + b_lr_2;
+
+% second level non-linear tranforms
+z_2 = g_2(a_l_2, a_r_2, a_lr_2);
+
+% third level activation value
+a_3 = w_3 * z_2 + b_3;
