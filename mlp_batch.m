91f5a67bff1906673e48af157d41a323897c0f76
Erol Can Ün <erolcan.un@gmail.com>
2013-05-09 13:58:08 +0200

    mlp_batch function definition

--- mlp_batch.m
+++ mlp_batch.m
@@ -1,5 +1,5 @@
 function [w_l_1, w_r_1, b_l_1, b_r_1, w_l_2, w_r_2, w_lr_2, b_l_2, b_r_2, w_3, b_3] = mlp_batch(X_L, X_R, T, X_L_val, X_R_val, T_val, h1, h2, nu, mu, batch_size)
-%MLP(X_L, X_R, T, h1, h2, nu, mu)
+%MLP_BATCH(X_L, X_R, T, X_L_val, X_R_val, T_val, h1, h2, nu, mu, batch_size)
 % X_L: Left input matrix
 % X_R: Right input matrix
 % T: Vector of classes of training inputs
