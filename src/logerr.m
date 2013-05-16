4e6dfbace5cfb3f4eea9f2f29010f2fcbafbab53
ferhat elmas <elmas.ferhat@gmail.com>
2013-05-16 21:19:19 +0200

    more concise error calculation

--- src/logerr.m
+++ src/logerr.m
@@ -5,10 +5,4 @@ function e_log = logerr(t, a_3)
 % e_log: logistic error for whole batch
 
 a = -t.*a_3;
-e_log = (a<0).*log1p(exp(a)) + (a>=0).*log(1+exp(a));
-
-% number of instances used in an epoch for training and validation datasets
-n = length(t);
-
-% calculate total error by averaging over error for each instance in epoch 
-e_log = sum(e_log)/n;
+e_log = mean((a<0).*log1p(exp(a)) + (a>=0).*log(1+exp(a)));
