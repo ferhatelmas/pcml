53ccb99f6af5f29605fa56772d6f3818a0d4c026
ferhat elmas <elmas.ferhat@gmail.com>
2013-05-21 18:49:32 +0300

    updated mlp_binary to call batch inside

--- src/mlp_binary.m
+++ src/mlp_binary.m
@@ -1,31 +1,34 @@
-function [logistic_error, zero_one_error] = mlp_binary(w_l_1, b_l_1, w_r_1, b_r_1, w_l_2, b_l_2, w_r_2, b_r_2, w_lr_2, b_lr_2, w_3, b_3)
-%MLP_BINARY(w_l_1, b_l_1, w_r_1, b_r_1, w_l_2, b_l_2, w_r_2, b_r_2, w_lr_2, b_lr_2, w_3, b_3)
-% Runs binary mlp with the given weights and bias
-% (w_l_1, b_l_1), (w_r_1, b_r_1): first level initial weight matrices and bias vectors
-% (w_l_2, b_l_2), (w_r_2, b_r_2), (w_lr_2, b_lr_2): second level initial weight matrices and bias vectors
-% (w_3, b_3): last level weight matrice and bias scalar
+function [logistic_error, zero_one_error] = mlp_binary(h1, h2, nu, mu, batch_size)
+%MLP_BINARY(h1, h2, nu, mu, batch_size)
+% h1: Number of neurons in the first layer
+% h2: Number of neurons in the second layer
+% nu: Learning rate
+% mu: Momentum term
+% batch_size: Number of input to be processed in one batch
 
-% Imports data into base workspace
-importfile('../data/norb_binary.mat');
+% Gets data from base workspace
+X_L = evalin('base', 'X_L');
+X_R = evalin('base', 'X_R');
+X_L_val = evalin('base', 'X_L_val');
+X_R_val = evalin('base', 'X_R_val');
+X_L_test = evalin('base', 'X_L_test');
+X_R_test = evalin('base', 'X_R_test');
+T = evalin('base', 'T');
+T_val = evalin('base', 'T_val');
+T_test = evalin('base', 'T_test');
 
-% Removes training data from base workspace
-clear GLOBAL train_cat_s train_left_s train_right_s
-
-% Gets test data into current workspace
-global test_cat_s test_left_s test_right_s
-
-% Maps class labels from {1, 3} to {-1, 1}
-test_cat_s = test_cat_s - 2;
+% Runs mlp_batch to get optimum weights and biases
+[w_l_1, w_r_1, b_l_1, b_r_1, ...
+ w_l_2, w_r_2, w_lr_2, b_l_2, b_r_2, b_lr_2, ...
+ w_3, b_3, ~, ~, ~] = mlp_batch(X_L, X_R, T, X_L_val, X_R_val, T_val, h1, h2, nu, mu, batch_size);
 
 % Does a forward pass to predict class labels
-[~, ~, ~, ~, ~, ~, ~, ~, a_3] = mlp_forward(test_left_s, test_right_s, ...
+[~, ~, ~, ~, ~, ~, ~, ~, a_3] = mlp_forward(X_L_test, X_R_test, ...
                                             w_l_1, b_l_1, w_r_1, b_r_1, ...
                                             w_l_2, b_l_2, w_r_2, b_r_2, w_lr_2, b_lr_2, ...
                                             w_3, b_3);
    
 % Calculates logistic and zero error for the predicted class labels                                        
-logistic_error = logerr(test_cat_s, a_3);
-zero_one_error = mean(T.*a_3 < 0);
-
-% Removes test data from base workspace
-clear GLOBAL test_cat_s test_left_s test_right_s
+logistic_error = logerr(T_test, a_3);
+zero_one_error = mean(T_test.*a_3 < 0);
+end
