0d1648696a5732125a2a497ec1e4e54c84f220aa
Erol Can Ün <erolcan.un@gmail.com>
2013-05-31 15:00:39 +0200

    logistic numu + early stopping
    early stopping: stops when last 3 errors are very close to each other

--- src/logistic_reg.m
+++ src/logistic_reg.m
@@ -26,17 +26,24 @@ tr_err = zeros(1,max_ec);
 val_err = zeros(1,max_ec);
 zero_one_err = zeros(1,max_ec);
 
-while(ec < max_ec)
-    ec = ec+1;
+early_stop = ones(1,2); % stores last two differences btw consecutive zero_one errors
+es = 0;
+prev_err = 0;
+
+% stop when three same or very close error values occur 3 in a row
+while(sum(early_stop > 1e-6*ones(1,2)) > 0)
+    es = mod(es + 1,2);
+    ec = ec+1
     Y = W*X;
     dW = ((exp(Y-repmat(lsexp(Y),k,1)) - T_T')*X')./n; % calculate gradient
     temp = gradient_descent(hdW, W, dW, nu, mu);
     hdW = W; W = temp;
     tr_err(ec) = logerr_multi(X,W,T_T); % training error with updated W
     val_err(ec) = logerr_multi(X_val,W,T_T_val); % validation error with updated W
-    disp(val_err(ec));
     [~,c] = max(W*X_val,[],1); % find index of maximum among each sample output
     zero_one_err(ec) = mean(c-1 ~= T_val);
+    early_stop(es+1) = abs(zero_one_err(ec) - prev_err);
+    prev_err = zero_one_err(ec);
     plotter(tr_err, val_err, zero_one_err, ec);
 end
 
--- src/multi_plotter.m
+++ src/multi_plotter.m
@@ -1,29 +1,29 @@
-function multi_semilogyter(tr_err_1, tr_err_2, tr_err_3, val_err_1, val_err_2, val_err_3, zero_one_1, zero_one_2, zero_one_3)
-%MULTI_semilogyTER(tr_err_1, tr_err_2, val_err_1, val_err_2, zero_one_1, zero_one_2)
+function multi_plotter(tr_err1, tr_err2, tr_err3, val_err1, val_err2, val_err3, zero_one_err1, zero_one_err2, zero_one_err3)
+%MULTI_semilogyTER(tr_err_1, tr_err_2, val_err_1, val_err_2, zero_one_err_1, zero_one_err_2)
 % semilogys two trials on the same figure
 % tr_err_i: vector of training errrors for each epoch for trial i
 % val_err_i: vector of validation errors for each epoch for trial i
-% zero_one_i: vector of zero-one errors for each epoch for trial i
+% zero_one_err_i: vector of zero-one errors for each epoch for trial i
 
 close all
 figure(1);
-semilogy(tr_err_1,'-r');
+semilogy(tr_err1,'-r');
 hold on
-semilogy(val_err_1,'--r');
+semilogy(val_err1,'--r');
 hold on
-semilogy(zero_one_1,'or');
+semilogy(zero_one_err1,'or');
 hold on
-semilogy(tr_err_2,'-b');
+semilogy(tr_err2,'-b');
 hold on
-semilogy(val_err_2,'--b');
+semilogy(val_err2,'--b');
 hold on
-semilogy(zero_one_2,'ob');
+semilogy(zero_one_err2,'ob');
 hold on
-semilogy(tr_err_3,'-k');
+semilogy(tr_err3,'-k');
 hold on
-semilogy(val_err_3,'--k');
+semilogy(val_err3,'--k');
 hold on
-semilogy(zero_one_3,'ok');
+semilogy(zero_one_err3,'ok');
 
 legend('Training 1','Validation 1','Zero-One 1','Training 2','Validation 2','Zero-One 2','Training 3','Validation 3','Zero-One 3');
 title('Training, Validation and Zero-One Errors for MLP Training');
