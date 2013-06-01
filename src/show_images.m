669819ca10f16e6fc6a50af3919a6c0c0f15f3b8
Erol Can Ün <erolcan.un@gmail.com>
2013-06-01 20:29:21 +0200

    updated results

--- src/show_images.m
+++ src/show_images.m
@@ -1,16 +1,16 @@
-% display images with y*a_3 closest to zero and largest negative
-x_l = X_L_val(:,min_i);
-x_r = X_R_val(:,min_i);
-x = (x_l + x_r)/2;
-x = reshape(x,24,24);
-figure(1);imshow(x,'InitialMagnification',500);
-title('Fig.1: Most Errored Image (t = 1)');
-tmin = T_val(min_i); 
+close all
 
-x_l = X_L_val(:,max_i);
-x_r = X_R_val(:,max_i);
+% display images with y*a_3 closest to zero and largest negative
+x_l = test_left_s(:,min_i);
+x_r = test_right_s(:,min_i);
 x = (x_l + x_r)/2;
-x = reshape(x,24,24);
-figure(2);imshow(x,'InitialMagnification',500);
-title('Fig.2: Least Errored Image (t = -1)');
-tmax = T_val(max_i);
+x = reshape(uint8(round(x - 1)),24,24);
+figure(1);imshow(x,'InitialMagnification',100);
+tmin = T_test(min_i); 
+
+x_l = test_left_s(:,max_i);
+x_r = test_right_s(:,max_i);
+x = (x_l + x_r)/2;
+x = reshape(uint8(round(x - 1)),24,24);
+figure(2);imshow(uint8(x),'InitialMagnification',100);
+tmax = T_test(max_i);
