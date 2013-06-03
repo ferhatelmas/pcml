32f4f3523b1a0f8f46d0e37b3a60bf16542d542f
ferhat elmas <elmas.ferhat@gmail.com>
2013-06-03 16:16:36 +0200

    multi_plotter.m -> plotter_multi.m

--- src/plotter_multi.m
+++ src/plotter_multi.m
@@ -1,5 +1,5 @@
-function multi_plotter(tr_err1, tr_err2, tr_err3, val_err1, val_err2, val_err3, zero_one_err1, zero_one_err2, zero_one_err3)
-%MULTI_semilogyTER(tr_err_1, tr_err_2, val_err_1, val_err_2, zero_one_err_1, zero_one_err_2)
+function plotter_multi(tr_err1, tr_err2, tr_err3, val_err1, val_err2, val_err3, zero_one_err1, zero_one_err2, zero_one_err3)
+%PLOTTER_MULTI(tr_err_1, tr_err_2, val_err_1, val_err_2, zero_one_err_1, zero_one_err_2)
 % semilogys two trials on the same figure
 % tr_err_i: vector of training errrors for each epoch for trial i
 % val_err_i: vector of validation errors for each epoch for trial i
